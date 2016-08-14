b0e64c0f50ebc6a1fd78e8178bf90f1dd01f0280
Marcin Pacholarz <marrcinpe@gmail.com>
2016-08-15 00:44:17 +0200

    init

---
+++ html/src/tk/marr/snake/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tk.marr.snake.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tk.marr.snake.SnakeGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SnakeGame();
+        }
+}
